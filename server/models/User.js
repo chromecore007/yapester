29cb25a12fefb328b02c3b6847ac458775d470ef
Prashant <157740185+chromecore007@users.noreply.github.com>
2026-06-19 19:33:35 +0530

    model

--- server/models/User.js
+++ server/models/User.js
@@ -25,6 +25,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    
+    profilePic: {
+  type: String,
+  default: "",
+},
   },
   { timestamps: true }
 );
