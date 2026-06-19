eb4086b67e58b4c10bfaead74bf57045b090803a
Prashant <157740185+chromecore007@users.noreply.github.com>
2026-06-19 19:36:31 +0530

    profile

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -15,6 +15,30 @@ router.get("/me", protect, async (req, res) => {
   }
 });
 
+router.put("/profile-pic", protect, async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      {
+        profilePic,
+      },
+      {
+        new: true,
+      }
+    ).select("-password");
+
+    res.json(user);
+  } catch (err) {
+    console.error(err);
+
+    res.status(500).json({
+      message: "Failed to update profile picture",
+    });
+  }
+});
+
 /* ================= ALL USERS ================= */
 router.get("/", protect, async (req, res) => {
   try {
